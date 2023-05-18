c4b79e340172922dd4759b8c044ca428d0245351
Chinemerem Daniel <danpresh302@gmail.com>
2023-05-18 15:32:11 +0100

    learned how to use the EXTENDS utility type

--- Lesson01/build/js/main.js
+++ Lesson01/build/js/main.js
@@ -337,3 +337,34 @@ console.log(isObj(true));
 console.log(isObj(Array(3)));
 console.log(isObj({ cow: "moo" }));
 console.log(isObj(undefined));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue("Danny"));
+console.log(isTrue(""));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({ name: "danny" }));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+console.log(isTrue(!null));
+console.log(isTrue(!!null));
+console.log(isTrue(!!undefined));
+console.log(isTrue(!undefined));
+const process = (user) => {
+    return user;
+};
+console.log(process({ id: 1, name: "Danny" })); // without the ID there would be an error
--- Lesson01/src/main.ts
+++ Lesson01/src/main.ts
@@ -428,3 +428,42 @@ console.log(isObj(true));
 console.log(isObj(Array(3)));
 console.log(isObj({ cow: "moo" }));
 console.log(isObj(undefined));
+
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false };
+  }
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { arg, is: false };
+  }
+  return { arg, is: !!arg };
+};
+
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue("Danny"));
+console.log(isTrue(""));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({ name: "danny" }));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+console.log(isTrue(!null));
+console.log(isTrue(!!null));
+console.log(isTrue(!!undefined));
+console.log(isTrue(!undefined));
+
+interface HasID {
+  id: number;
+}
+
+const process = <T extends HasID>(user: T): T => {
+  return user;
+};
+
+console.log(process({ id: 1, name: "Danny" })); // without the ID there would be an error
