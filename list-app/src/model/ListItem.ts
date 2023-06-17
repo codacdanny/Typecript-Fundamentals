92b8af9cf967dce34b678987367d6175a4a7d869
Chinemerem Daniel <danpresh302@gmail.com>
2023-06-17 22:17:08 +0100

    building interface for the listitems

--- list-app/src/model/ListItem.ts
+++ list-app/src/model/ListItem.ts
@@ -3,3 +3,29 @@ export interface Item {
   item: string;
   checked: boolean;
 }
+
+export default class ListItem implements Item {
+  constructor(
+    private _id: string = "",
+    private _item: string = "",
+    private _checked: boolean = false
+  ) {}
+  get id(): string {
+    return this.id;
+  }
+  set id(id: string) {
+    this._id = id;
+  }
+  get item(): string {
+    return this.item;
+  }
+  set item(item: string) {
+    this._item = item;
+  }
+  get checked(): boolean {
+    return this.checked;
+  }
+  set checked(checked: boolean) {
+    this._checked = checked;
+  }
+}
